9287ae55b1ed44da2532b325998a7916bca8df82
lmeller-git <louis.meller@icloud.com>
2026-07-16 11:11:54 +0200

    add some plumbing

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// #![deny(missing_docs)]
+#![deny(clippy::missing_safety_doc, clippy::undocumented_unsafe_blocks)]
+#![warn(unsafe_op_in_unsafe_fn)]
+#![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[cfg(feature = "std")]
+extern crate std;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+mod sync;
+
+pub struct Storage {}
+
+impl Storage {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    pub fn pull(&self) -> Option<usize> {
+        todo!()
+    }
+
+    pub fn put(&self, index: usize) -> Result<(), usize> {
+        todo!()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        todo!()
+    }
+
+    pub fn is_full(&self) -> bool {
+        todo!()
+    }
+
+    pub fn len(&self) -> usize {
+        todo!()
+    }
+
+    pub fn capacity(&self) -> usize {
+        todo!()
     }
 }
